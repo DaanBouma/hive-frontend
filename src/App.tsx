5127d96874fc166a45b4713418b1ae0f3ea2bc57
Daan Bouma <130249739+DaanBouma@users.noreply.github.com>
2025-11-27 11:33:20 +0100

    funny animation testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
+import { DotLottieReact } from '@lottiefiles/dotlottie-react';
+import "./style.css"
+
 function App() {
   return (
     <>
-    Bijen horen erbij!
+    <div className="App">
+    <DotLottieReact
+    className="App_logo"
+      src="https://lottie.host/d1cacb3c-bac3-4410-9962-283582f762a6/Rq1jOYB4F7.lottie"
+      loop
+      autoplay
+    />
+    <h1>Hive - Bee Management</h1>
+    </div>
     </>
   )
 }
