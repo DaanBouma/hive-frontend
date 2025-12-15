7d16d63478017fb7965982658c4ffd65780feffd
Daan Bouma <130249739+DaanBouma@users.noreply.github.com>
2025-12-15 11:47:53 +0100

    Router updated so that routes are better

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,16 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Dashboard from "./pages/Dashboard/Dashboard"
-import { HiveList, HiveDetail } from "./pages/Hives"
+import { HiveList, HiveDetail} from "./pages/Hives"
 import {
   InspectionList,
   InspectionView,
   InspectionForm
 } from "./pages/Inspections"
+import "./style.css"
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Dashboard />} />
-
         <Route path="/hives" element={<HiveList />} />
         <Route path="/hives/:hiveId" element={<HiveDetail />} />
 
--- src/pages/Hives/HiveList.tsx
+++ src/pages/Hives/HiveList.tsx
@@ -3,14 +3,33 @@ import { Card, Header } from "../../components"
 function HiveList() {
   return (
     <div className="App">
-      <Header title="Bijenkasten" description="Kies een bijenkast" />
+      <Header title="Bijen horen erbij" description="Kies jouw bijenkast" />
       <div className="Body">
         <Card
           name="Bijenkast 1"
           subname="Laatste inspectie"
           date="20/05/2025 20:00"
           buttons={[
-            { type: "primary", text: "Bekijk kast", href: "/hives/1" }
+            { type: "primary", text: "Bekijk kast", href: "/hives/1" },
+            { type: "secondary", text: "Nieuwe inspectie", href: "/hives/1/inspections/new" }
+          ]}
+        />
+        <Card
+          name="Bijenkast 2"
+          subname="Laatste inspectie"
+          date="20/05/2025 20:00"
+          buttons={[
+            { type: "primary", text: "Bekijk kast", href: "/hives/2" },
+            { type: "secondary", text: "Nieuwe inspectie", href: "/hives/1/inspections/new" }
+          ]}
+        />
+        <Card
+          name="Bijenkast 3"
+          subname="Laatste inspectie"
+          date="20/05/2025 20:00"
+          buttons={[
+            { type: "primary", text: "Bekijk kast", href: "/hives/3" },
+            { type: "secondary", text: "Nieuwe inspectie", href: "/hives/1/inspections/new" }
           ]}
         />
       </div>
